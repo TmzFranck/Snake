74254a23d6867ca561a33e5cc0d899364846a2ad
Franck Boudouin Tameze <francktameze5@gmail.com>
2024-06-16 03:02:52 +0200

    add main loop for the game

--- main.py
+++ main.py
@@ -57,3 +57,17 @@ class Food(object):
 
 
 
+def main():
+    while True:
+        pass
+        # clear screen
+        # display the snake
+        # display the food
+        # display the score
+        # listen to keypress event
+        # respond to keypress event
+        # stop the game if the head hits the body (eat itself)
+
+
+if __name__ == "__main__":
+    main()
